3dfbef712ac9b215e36cc7dae3dc02d4f1fdfbdb
jcnizama <shoes@LAPTOP-CMRLAF0I>
2022-09-15 17:08:58 -0500

    Primer commit

---
+++ Documents/NetBeansProjects/Ejercicios/src/main/java/HolaMundo.java
@@ -0,0 +1,11 @@
+
+public class HolaMundo {
+    
+    public static void main (String[] args){
+        System.out.println("Hola kaumza");
+        System.out.println("g");
+        System.out.println("");
+        System.out.println("");
+       
+    }
+}
